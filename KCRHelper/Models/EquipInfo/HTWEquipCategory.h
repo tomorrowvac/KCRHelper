bd5377b4c9fc4656dec7acf040ae069869040ef9
workeramo <workeramo@gmail.com>
2015-06-05 18:09:21 +0800

    test2

---
+++ KCRHelper/Models/EquipInfo/HTWEquipCategory.h
@@ -0,0 +1,13 @@
+//
+//  HTWEquipCategory.h
+//  KCRHelper
+//
+//  Created by workeramo on 15/6/5.
+//  Copyright (c) 2015年 HenTaiWorkerspace. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HTWEquipCategory : NSObject
+
+@end
