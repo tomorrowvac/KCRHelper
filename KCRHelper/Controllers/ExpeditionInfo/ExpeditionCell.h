6f0fe2e0ee66579de2f3a08109a6037d3d6df0d4
tomorrowvac <vachelandtim@126.com>
2015-06-06 13:48:48 +0800

    20150606

---
+++ KCRHelper/Controllers/ExpeditionInfo/ExpeditionCell.h
@@ -0,0 +1,15 @@
+//
+//  ExipeditionCell.h
+//  KCRHelper
+//
+//  Created by Vachel on 15/6/6.
+//  Copyright (c) 2015年 HenTaiWorkerspace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExpeditionCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *expeditionLabel;
+
+
+@end
---
+++ KCRHelper/Controllers/ExpeditionInfo/ExpeditonDetailController.h
@@ -0,0 +1,13 @@
+//
+//  ExpeditonDetailController.h
+//  KCRHelper
+//
+//  Created by Vachel on 15/6/6.
+//  Copyright (c) 2015年 HenTaiWorkerspace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExpeditonDetailController : UIViewController
+
+@end
